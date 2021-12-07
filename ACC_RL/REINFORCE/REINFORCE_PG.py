ea41a46eeea6e0e978eb2ffbdb9ba7afd8a720ec
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2021-12-06 20:14:59 -0500

    Update REINFORCE_PG.py

--- ACC_RL/REINFORCE/REINFORCE_PG.py
+++ ACC_RL/REINFORCE/REINFORCE_PG.py
@@ -6,7 +6,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.distributions import Normal
-import test_REINFORCE
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
