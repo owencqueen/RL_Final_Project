c5b954bad41bd63fed887b36dc82369dff743133
Tim Thacker <47113711+TimThacker@users.noreply.github.com>
2021-12-06 23:07:25 -0500

    Update test_REINFORCE.py

--- ACC_RL/test_scripts/test_REINFORCE.py
+++ ACC_RL/test_scripts/test_REINFORCE.py
@@ -73,8 +73,8 @@ def main():
 
         while not done:
             action, log_prob = policy.select_action(np.array(state))
-            #action = action.astype(np.double))
-            action = torch.squeeze(action)
+            action = action.astype(np.double))
+            #action = torch.squeeze(action)
             next_state = env.step(action)
             reward = get_reward(next_state)
             next_state = torch.autograd.Variable(torch.from_numpy(next_state)).float()
